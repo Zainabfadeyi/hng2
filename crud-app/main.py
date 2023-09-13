0168a607411f7d298439f9a0fdd0abc7ecd1a02e
Zainab omolola fadeyi <zainabfadeyi@gmail.com>
2023-09-13 08:36:04 +0100

    mu

--- crud-app/main.py
+++ crud-app/main.py
@@ -1,41 +1,80 @@
+# # main.py
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+# class Person(BaseModel):
+#     name: str
+#     age: int
+#     email: str
+
+# # In-memory database (for demonstration purposes)
+# db = []
+
+# @app.post("/api/person")
+# async def create_person(person: Person):
+#     db.append(person)
+#     return {"message": "Person created successfully"}
+
+# @app.get("/api/person/{person_id}")
+# async def read_person(person_id: int):
+#     if person_id < len(db):
+#         return db[person_id]
+#     else:
+#         return {"error": "Person not found"}
+
+# @app.put("/api/person/{person_id}")
+# async def update_person(person_id: int, updated_person: Person):
+#     if person_id < len(db):
+#         db[person_id] = updated_person
+#         return {"message": "Person updated successfully"}
+#     else:
+#         return {"error": "Person not found"}
+
+# @app.delete("/api/person/{person_id}")
+# async def delete_person(person_id: int):
+#     if person_id < len(db):
+#         db.pop(person_id)
+#         return {"message": "Person deleted successfully"}
+#     else:
+#         return {"error": "Person not found"}    
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+# Create a FastAPI instance
 app = FastAPI()
 
-class Person(BaseModel):
+# Define Pydantic models for request and response
+class PersonBase(BaseModel):
     name: str
     age: int
     email: str
 
-# In-memory database (for demonstration purposes)
-db = []
+class PersonCreate(PersonBase):
+    pass
 
-@app.post("/api/person")
-async def create_person(person: Person):
-    db.append(person)
-    return {"message": "Person created successfully"}
+class Person(PersonBase):
+    id: int
 
-@app.get("/api/person/{person_id}")
-async def read_person(person_id: int):
-    if person_id < len(db):
-        return db[person_id]
-    else:
-        return {"error": "Person not found"}
+# Define CRUD operations using SQLAlchemy and FastAPI
+@app.post("/api/person", response_model=Person)
+def create_person(person: PersonCreate):
+    # Implement your create logic here
+    pass
 
-@app.put("/api/person/{person_id}")
-async def update_person(person_id: int, updated_person: Person):
-    if person_id < len(db):
-        db[person_id] = updated_person
-        return {"message": "Person updated successfully"}
-    else:
-        return {"error": "Person not found"}
+@app.get("/api/person/{person_id}", response_model=Person)
+def read_person(person_id: int):
+    # Implement your read logic here
+    pass
 
-@app.delete("/api/person/{person_id}")
-async def delete_person(person_id: int):
-    if person_id < len(db):
-        db.pop(person_id)
-        return {"message": "Person deleted successfully"}
-    else:
-        return {"error": "Person not found"}    
+@app.put("/api/person/{person_id}", response_model=Person)
+def update_person(person_id: int, updated_person: PersonBase):
+    # Implement your update logic here
+    pass
+
+@app.delete("/api/person/{person_id}", response_model=Person)
+def delete_person(person_id: int):
+    # Implement your delete logic here
+    pass
